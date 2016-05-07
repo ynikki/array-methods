b4109ef64d6e2669c7eb26c25607772567f040d3
Nikki Y Kobayashi <nikz@MacBook-Pro-2.local>
2016-05-07 11:26:14 -1000

    pop the last element

--- arrayMethods.js
+++ arrayMethods.js
@@ -21,3 +21,5 @@ var bulkNumbers = [5, 5, 6, 6, 9, 9, 2, 2, 2, 5, 2];
 var nameArray = [];
 console.log(nameArray.push('Nikki','Kobayashi'));
 console.log(genericNumberArray.push(6,7,8,9,10));
+
+var lastElement = colors.pop('Charlie');
