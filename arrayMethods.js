3755443f53eb1ff3afebdfd858064e3a6ae5bf2b
Nikki Y Kobayashi <nikz@MacBook-Pro-2.local>
2016-05-07 11:29:13 -1000

    reverse phoneNumber

--- arrayMethods.js
+++ arrayMethods.js
@@ -23,7 +23,7 @@ console.log(nameArray.push('Nikki','Kobayashi'));
 console.log(genericNumberArray.push(6,7,8,9,10));
 
 var lastElement = colors.pop('Charlie');
-
 var popResult = lastElement;
 
 scrambledWords.reverse();
+phoneNumber.reverse();
